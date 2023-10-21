694824ac5adc85c0a06cac4b7846994def52355e
chase coogan <chasecoogan12@gmail.com>
2023-10-21 18:21:54 -0400

    parseJSON func built

--- cmd/main.go
+++ cmd/main.go
@@ -1,44 +1,13 @@
 package main
 
 import (
-	"encoding/json"
+	"backend/internal/parse"
+	"backend/internal/types"
 	"fmt"
-	"io/ioutil"
-	"log"
-	"net/http"
-	"os"
-	// "github.com/gin-gonic/gin"
 )
 
-type SpaceCraft struct {
-	Name string `json:"name"`
-	ID   int    `json:"ID"`
-}
-
-type parseJSON struct {
-	SpaceCrafts []SpaceCraft
-}
-
 func main() {
-	response, err := http.Get("https://isro.vercel.app/api/spacecrafts")
-
-	if err != nil {
-		fmt.Print(err.Error())
-		os.Exit(1)
-	}
-
-	responseData, err := ioutil.ReadAll(response.Body)
-
-	var resp parseJSON // json resp
-	json.Unmarshal(responseData, &resp)
-
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	for _, each := range resp.SpaceCrafts {
-		fmt.Printf("ID %d: Name %s: \n", each.ID, each.Name)
-	}
-
-	// fmt.Println(string(responseData))
+	ret := parse.GrabAPI("https://isro.vercel.app/api/spacecrafts")
+	ans := types.CreateSpaceCraft(ret)
+	fmt.Println(ans)
 }
