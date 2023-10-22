5cd55c396d0549fac7d512eaf21b56d2924a360e
chase coogan <chasecoogan12@gmail.com>
2023-10-22 01:54:37 -0400

    Redis database setup and data sent

--- cmd/main.go
+++ cmd/main.go
@@ -4,36 +4,18 @@ import (
 	"backend/internal/database"
 	"backend/internal/parse"
 	"backend/internal/types"
-	"fmt"
 )
 
 func main() {
-	database.CreateDatabase()
+	// database.CreateDatabase()
 
-	// space_crafts := parse.GrabAPI("https://isro.vercel.app/api/spacecrafts")
-	// build_space_crafts := types.CreateSpaceCraft(space_crafts)
-	// fmt.Println(build_space_crafts)
+	space_crafts := parse.GrabAPI("https://api.spacexdata.com/v3/payloads")
+	// pretty.Println(string(space_crafts))
+	build_space_crafts := types.CreateDonki(space_crafts)
 
-	// launchers := parse.GrabAPI("https://isro.vercel.app/api/launchers")
-	// build_launchers := types.CreateLauncher(launchers)
-	// fmt.Println(build_launchers)
+	// pretty.Println(build_space_crafts)
 
-	// satellite := parse.GrabAPI("https://isro.vercel.app/api/customer_satellites")
-	// build_sattelites := types.CreateSattelite(satellite)
-	// fmt.Println(build_sattelites)
-
-	centre := parse.GrabAPI("https://isro.vercel.app/api/centres")
-	build_centre := types.CreateCentre(centre)
-	fmt.Println(build_centre)
-
-	// database.InsertIntoTable("centres", build_centre)
-	// database.QueryTable("centres")
-
-	launchers := parse.GrabAPI("https://isro.vercel.app/api/launchers")
-	build_launcher := types.CreateLauncher(launchers)
-	// fmt.Println(build_launcher)
-
-	database.InsertIntoTable("launchers", build_launcher)
-	database.QueryTable("launchers")
+	database.InsertIntoTable("spacex_payloads", build_space_crafts)
+	database.QueryTable("spacex_payloads")
 
 }
