40ea821cded058851c32992571a42ac79347a72e
chase coogan <chasecoogan12@gmail.com>
2023-10-22 03:01:20 -0400

    send .env

--- internal/parse/parse.go
+++ internal/parse/parse.go
@@ -1,5 +1,25 @@
 package parse
 
-func grabAPI() {
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+)
 
+func GrabAPI(url string) []byte {
+	response, err := http.Get(url)
+
+	if err != nil {
+		fmt.Print(err.Error())
+		os.Exit(1)
+	}
+	responseData, err := ioutil.ReadAll(response.Body)
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	return responseData
 }
