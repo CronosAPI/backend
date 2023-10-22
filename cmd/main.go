5d4d2d8edaec7205cf970c37799379b89532e7d2
chase coogan <chasecoogan12@gmail.com>
2023-10-22 02:51:36 -0400

    .env update to handle api deployment

--- cmd/main.go
+++ cmd/main.go
@@ -1,21 +1,17 @@
 package main
 
-import (
-	"backend/internal/database"
-	"backend/internal/parse"
-	"backend/internal/types"
-)
+import "backend/internal/database"
 
 func main() {
 	// database.CreateDatabase()
 
-	space_crafts := parse.GrabAPI("https://api.spacexdata.com/v3/payloads")
+	// space_crafts := parse.GrabAPI("https://api.spacexdata.com/v3/payloads")
 	// pretty.Println(string(space_crafts))
-	build_space_crafts := types.CreateDonki(space_crafts)
+	// build_space_crafts := types.CreateDonki(space_crafts)
 
 	// pretty.Println(build_space_crafts)
 
-	database.InsertIntoTable("spacex_payloads", build_space_crafts)
-	database.QueryTable("spacex_payloads")
+	// database.InsertIntoTable("spacex_payloads", build_space_crafts)
+	database.QueryTable("satellites")
 
 }
