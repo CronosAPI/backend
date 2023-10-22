e24d2fc447d24e35cc81281a0ec5b2d68773e00f
chase coogan <chasecoogan12@gmail.com>
2023-10-21 23:08:27 -0400

    database tables created

--- cmd/main.go
+++ cmd/main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"backend/internal/database"
+	"backend/internal/parse"
+	"backend/internal/types"
+	"fmt"
 )
 
 func main() {
@@ -19,8 +22,18 @@ func main() {
 	// build_sattelites := types.CreateSattelite(satellite)
 	// fmt.Println(build_sattelites)
 
-	// centre := parse.GrabAPI("https://isro.vercel.app/api/centres")
-	// build_centre := types.CreateCentre(centre)
-	// fmt.Println(build_centre)
+	centre := parse.GrabAPI("https://isro.vercel.app/api/centres")
+	build_centre := types.CreateCentre(centre)
+	fmt.Println(build_centre)
+
+	// database.InsertIntoTable("centres", build_centre)
+	// database.QueryTable("centres")
+
+	launchers := parse.GrabAPI("https://isro.vercel.app/api/launchers")
+	build_launcher := types.CreateLauncher(launchers)
+	// fmt.Println(build_launcher)
+
+	database.InsertIntoTable("launchers", build_launcher)
+	database.QueryTable("launchers")
 
 }
